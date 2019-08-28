2dfc4887db42b61bd3196b4d7ead4e9685849ae9
Mert Ertuğrul <gmertugrulmert@users.noreply.github.com>
2019-08-28 15:25:05 +0300

    Car direction, game over animation
    Car direction change mechanism has become smoother, the game over animation is taking shape

--- app/src/main/java/com/mertugrul/cargame01/MainActivity.java
+++ app/src/main/java/com/mertugrul/cargame01/MainActivity.java
@@ -1,30 +1,39 @@
 package com.mertugrul.cargame01;
 
 import java.lang.Math;
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
+
+import com.google.ar.core.Frame;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
 import com.google.ar.core.TrackingState;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Camera;
 import com.google.ar.sceneform.FrameTime;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
-import com.google.ar.sceneform.collision.Box;
 import com.google.ar.sceneform.math.Quaternion;
 import com.google.ar.sceneform.math.QuaternionEvaluator;
 import com.google.ar.sceneform.math.Vector3;
@@ -32,16 +41,24 @@ import com.google.ar.sceneform.math.Vector3Evaluator;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     private ArFragment arFragment;
+    private Anchor startAnchor = null;
+    private Node startNode;
     private Node carNode;
-    private AnchorNode anchornode;
+    private AnchorNode anchornode = null;
     private Node[] stararray = new Node[10];
     private final float BOARDSIZE = 1.2f;
+    private Scene scene;
+    private Camera camera;
+
+
+    private CountDownTimer gameTimer;
     private int count = 0;
     private int score = 0;
     private TextView scoreText;
+    private TextView timeText;
     private boolean createdGame = false;
     private MediaPlayer mp;
 
@@ -49,8 +66,13 @@ public class MainActivity extends AppCompatActivity  {
     Joystick joystick;
     private double direction = 0;
     private Handler directionHandler;
-    private ObjectAnimator drive;
     Vector3 increment = new Vector3(0,0,0);
+
+    private ObjectAnimator fall;
+    private ObjectAnimator drive;
+
+    private Quaternion turnQuaternion;
+    private float turnAngle;
 
 
     // orientation values
@@ -68,17 +90,35 @@ public class MainActivity extends AppCompatActivity  {
         @Override
         public void run() {
             try {
+                /**if( arFragment.getArSceneView().getArFrame() != null &&!createdGame && startAnchor == null
+                        && arFragment.getArSceneView().getArFrame().getCamera().getTrackingState() == TrackingState.TRACKING )  {
+                    startScreen();
+                     System.out.println("inside if"); }
+                **/
                 if( createdGame && joystick != null && carNode != null) {
-                    direction = joystick.getDegrees();
-                    increment = new Vector3((float) (Math.sin(direction)),0,(float)  (Math.cos(direction)));
-                    carNode.setLookDirection(new Vector3((float) (-1*Math.cos(direction)),0,(float)  (Math.sin(direction))));
-                    System.out.println("increment:" + increment.toString() + "location:"+carNode.getWorldPosition().toString());
+
                     if (drive != null && joystick.getMoving()) {
+                        direction = joystick.getDegrees();
+                        increment = new Vector3((float) (Math.sin(direction)),0,(float)  (Math.cos(direction)));
                         drive.end();
-                        drive.setObjectValues(carNode.getWorldPosition(), Vector3.add(carNode.getWorldPosition(), increment.scaled(0.00035f)));
+                        drive.setObjectValues(carNode.getWorldPosition(), Vector3.add(carNode.getWorldPosition(), carNode.getLeft().scaled(0.003f)));
                         //carNode.setWorldRotation(Quaternion.axisAngle(new Vector3(0.0f, 1.0f, 0.0f), (float)direction));
                       //  if (joystick.getMoving())
+
+                       turnAngle = Vector3.angleBetweenVectors(carNode.getLeft(),increment);
+                       if (Vector3.cross(carNode.getLeft(),increment).y < 0)
+                           turnAngle = -turnAngle;
+                       turnAngle *= 0.009f;
+                       turnQuaternion = Quaternion.multiply(carNode.getLocalRotation(),new Quaternion(new Vector3(0,1.0f,0),turnAngle));
+                        carNode.setLocalRotation(turnQuaternion);
+
                         drive.start();
+
+
+                        System.out.println("increment:" + increment.toString() + "location:"+carNode.getWorldPosition().toString());
+
+
+
                     }
                     ArrayList<Node> overlappedNodes = arFragment.getArSceneView().getScene().overlapTestAll(carNode);
                     for (Node node : overlappedNodes) {
@@ -104,16 +144,16 @@ public class MainActivity extends AppCompatActivity  {
                 }
             } finally {
 
-                directionHandler.postDelayed(directionChecker, 1);
+                directionHandler.postDelayed(directionChecker, 3);
             }
         }
     };
-
 
     @Override
     public void onBackPressed() {
             //Remove an anchor node
             if (anchornode != null) {
+                gameTimer.cancel();
                 arFragment.getArSceneView().getScene().removeChild(anchornode);
                 anchornode.getAnchor().detach();
                 anchornode.setParent(null);
@@ -121,10 +161,13 @@ public class MainActivity extends AppCompatActivity  {
                 createdGame = false;
                 directionHandler.removeCallbacks(directionChecker);
                 drive.end();
+                startScreen();
+                score=0;
+                scoreText.setText("");
+                timeText.setText("");
 
-                Toast.makeText(this, "Anchornode deleted", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(this, "Anchornode already null", Toast.LENGTH_SHORT).show();
+                System.out.println("Quitting game");
                 super.onBackPressed();
             }
     }
@@ -135,19 +178,42 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
         mp= MediaPlayer.create(getApplicationContext(), R.raw.money);
         mp.setVolume(1, 1);
+
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.arFragment);
         scoreText = findViewById(R.id.textView);
-        scoreText.setText("Score:" + score);
+        scoreText.setTextColor(Color.WHITE);
+        scoreText.setText("");
+        timeText = findViewById(R.id.textView2);
+        timeText.setTextColor(Color.WHITE);
+        timeText.setText("");
+        gameTimer = new CountDownTimer(10000,1000) {
+            @Override
+            public void onTick(long l) {
+                timeText.setText("Time Left: "+ l/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                drive.end();
+                createdGame = false;
+                endScreen();
+            }
+        };
+
+        scene = arFragment.getArSceneView().getScene();
+        camera = scene.getCamera();
+        startScreen();
 
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
             if (!createdGame) {
                 createdGame = true;
+                scoreText.setText("Score:" + score);
+                startNode.setParent(null);
                 Anchor anchor = hitResult.createAnchor();
                 anchornode = new AnchorNode(anchor);
-
+                score = 0;
                 //rendering the car
                 ModelRenderable.builder()
                         .setSource(this, Uri.parse("car_02.sfb"))
@@ -165,40 +231,76 @@ public class MainActivity extends AppCompatActivity  {
                 arFragment.getArSceneView().getScene().addChild(anchornode);
                 //create joystick
                 joystick = findViewById(R.id.joystick);
+                gameTimer.start();
                 directionHandler = new Handler();
                 directionChecker.run();
             }
         });
 
+
+
+    }
+
+    private void endScreen() {
+        drive.end();
+        Node endnode = new Node();
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("end.sfb"))
+                .build()
+                .thenAccept(modelRenderable -> {
+                    endnode.setRenderable(modelRenderable);
+                    anchornode.addChild(endnode);
+                })
+                .exceptionally(throwable -> {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(throwable.getMessage())
+                            .show();
+                    return null;
+                });
+        endnode.setLocalPosition(Vector3.add(carNode.getLocalPosition(),new Vector3(0f,1.0f,0)));
+        endnode.setLocalRotation(Quaternion.axisAngle(new Vector3(1.0f, 0.0f, 0), -90f));
+        fall = new ObjectAnimator();
+        fall.setObjectValues(endnode.getLocalPosition(),Vector3.add(carNode.getLocalPosition(),new Vector3(0.059f,0,0.22f)));
+        fall.setPropertyName("localPosition");
+        fall.setEvaluator(new Vector3Evaluator());
+        fall.setRepeatCount(0);
+        fall.setInterpolator(new AccelerateDecelerateInterpolator());
+        fall.setDuration(1000);
+        fall.setTarget(endnode);
+        fall.start();
+
     }
 
 
-    /**private void startScreen(){
-         Anchor startAnchor;
-        for (Plane plane : arFragment.getArSceneView().getSession().getAllTrackables(Plane.class)) {
-            if (plane.getType() == Plane.Type.HORIZONTAL_UPWARD_FACING
-                && plane.getTrackingState() == TrackingState.TRACKING)
-            {
-             startAnchor = plane.createAnchor(plane.getCenterPose());
-                break;
-            }
-        }
-        if (startAnchor == null)return;
-        else {
-            anchornode = new AnchorNode(startAnchor);
-            ModelRenderable.builder()
-                    .setSource(this, Uri.parse("car_02.sfb"))
-                    .build()
-                    .thenAccept(modelRenderable -> addCar(modelRenderable))
-                    .exceptionally(throwable -> {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                        builder.setMessage(throwable.getMessage())
-                                .show();
-                        return null;
-                    });
+    private void startScreen(){
+        Plane plane;
+       // if (arFragment.getArSceneView().getArFrame().getUpdatedAnchors().iterator().hasNext())
+          //  Anchor = arFragment.getArSceneView().getArFrame().getUpdatedTrackables(Class Plane).iterator().next();
+        System.out.println("inside start screen");
+        Pose pose = Pose.makeTranslation(0.0f,0.0f,0.5f);
 
+                ModelRenderable.builder()
+                        .setSource(this, Uri.parse("tinker.sfb"))
+                        .build()
+                        .thenAccept(modelRenderable -> {
+
+                            startNode = new Node();
+                            startNode.setRenderable(modelRenderable);
+                            scene.addChild(startNode);
+                            startNode.setLocalPosition(Vector3.add(Vector3.add(camera.getLocalPosition(),camera.getDown()),camera.getForward()));
+                            startNode.setLocalRotation(Quaternion.axisAngle(new Vector3(1.0f, 0.0f, 0), -80f));
+
+
+
+                        })
+                        .exceptionally(throwable -> {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setMessage(throwable.getMessage())
+                                    .show();
+                            return null;
+                        });
+                System.out.println("start built");
         }
-    }**/
 
 
 
@@ -218,7 +320,6 @@ public class MainActivity extends AppCompatActivity  {
                         return null;
                     });
         }
-
     }
 
     private void addCar(ModelRenderable modelRenderable) {
